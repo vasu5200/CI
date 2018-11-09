47ffc35fa3084afbffea765db9b99a02c7677c64
vasiredddy S <ss.sankar520@gmail.com>
2018-11-09 21:22:18 +0530

    Task_ Accionlabs
    Function that returns True if a list can partitioned else False.

---
+++ Task_ Accionlabs.py
@@ -0,0 +1,26 @@
+def parti(arbitrarylist):
+    '''Function that returns True if the list can be partitioned and False if it is not.'''
+    if sum(arbitrarylist) % 2 == 0 : #if it is not a even sum then we can't divide it into 2 partitions
+
+        listone = []#temporory partition one, Not necessary but useful to debug and optimize the function further
+        listtwo = []#temporory partition two
+        listonesum = 0
+        listtwosum = 0
+
+        for i in sorted(list1, reverse=True):#actual logic
+
+            if listonesum > listtwosum:
+                listtwosum += i
+                listtwo.append(i)
+            else:
+                listonesum += i
+                listone.append(i)
+        print(listone, listtwo)
+
+        if  listonesum == listtwosum:#checking if both list's sum is same or not
+            return True
+        else:
+            return False
+
+    else:
+        return False
